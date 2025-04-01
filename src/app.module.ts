6f52a1a7631cdcc7f53297d40589e42db35e8e3a
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-03-31 21:39:17 -0300

    Integração do Redis e TypeOrm na aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InvoiceModule } from './invoice/invoice.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? 'secret',
+      database: process.env.DB_NAME ?? 'energy_invoices',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: () => ({
+        store: redisStore,
+        host: process.env.REDIS_HOST ?? 'localhost',
+        port: parseInt(process.env.REDIS_PORT ?? '6379'),
+        ttl: 60 * 60, // 1 hora em segundos
+      }),
+    }),
+    InvoiceModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
